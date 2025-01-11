e66f4f3158b0cf72c991d70b2b6a7febb418775b
versatileYT <106322999+versatileYT@users.noreply.github.com>
2025-01-11 23:56:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://gdhzpqaskoyvbfypfkfv.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkaHpwcWFza295dmJmeXBma2Z2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY2Mjg3MjIsImV4cCI6MjA1MjIwNDcyMn0.eAe2kQUxRRin9WPjSCB9JyHGhPtUmBt4tyk-IkIRvD8
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 let balance = 1000;
